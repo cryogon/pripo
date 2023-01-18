6d25374a29d4f848203ae593b965acee1c929ec1
JatinThakur <jtkalga@gmail.com>
2023-01-18 16:33:37 +0530

    Fix: Authorization Failed In Case Off Token Expiration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
+import { useAuth0 } from "@auth0/auth0-vue";
+import { onMounted } from "vue";
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
+const { isAuthenticated } = useAuth0();
+
+onMounted(() => {
+  setTimeout(() => {
+    if (!isAuthenticated) {
+      localStorage.removeItem("token");
+      window.location.reload();
+    }
+  }, 500);
+});
 </script>
 
 <template>
