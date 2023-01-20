334734e68cce400447e6f7bdb33edc6045897abf
JatinThakur <jtkalga@gmail.com>
2023-01-20 17:47:41 +0530

    Added: Share Icon

---
+++ src/components/Icons/ShareIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    width="32"
+    height="42"
+    viewBox="0 0 32 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="25" cy="7" r="6.5" stroke="black" />
+    <circle cx="7" cy="21" r="6.5" stroke="black" />
+    <circle cx="25" cy="35" r="6.5" stroke="black" />
+    <line x1="11.7" y1="16.6" x2="19.7" y2="10.6" stroke="black" />
+  </svg>
+</template>
+<style scoped>
+circle,
+line {
+  stroke: var(--color-text);
+}
+</style>
