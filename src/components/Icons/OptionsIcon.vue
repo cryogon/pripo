4e839e09a8cf640d542f79654ad43e6a4bebb62d
JatinThakur <jtkalga@gmail.com>
2023-01-25 12:54:30 +0530

    Added: New Icons

---
+++ src/components/Icons/OptionsIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="14"
+    height="52"
+    viewBox="0 0 14 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="7" cy="7" r="6.5" stroke="black" />
+    <circle cx="7" cy="27" r="6.5" stroke="black" />
+    <circle cx="7" cy="45" r="6.5" stroke="black" />
+  </svg>
+</template>
+<style scoped>
+circle {
+  stroke: var(--color-text);
+}
+</style>
---
+++ src/components/Icons/ReplyIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="77"
+    height="72"
+    viewBox="0 0 77 72"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M58 70L58.5 70.5L59.5 71H61L64 69L68.5 64.5L71.5 60.5L74 55.5L75.5 50L76 45L75.5 39L74.5 34.5L72.5 30.5L70 26L67 23L63 20L57.5 17.5L50.5 16.1111H34V5L33.5 4L33 3L32 2L30 1H28.5L26.5 2L4.5 23L2 26L1 28V29L1.5 30.5L2 31.5L26 55L28 56H30L32 55L33 53.5L33.5 52.5L33.8173 51.2346V41H48.5H50L53 42L55 43L57 44.5L59 46.5L60.5 49L61.5 52L62 55V57L61.5 60L61 61.5L60 63.5L59 65L58 66L57.5 67V68V69L58 70Z"
+      stroke="black"
+    />
+  </svg>
+</template>
+<style scoped>
+path {
+  stroke: var(--color-text);
+}
+</style>
