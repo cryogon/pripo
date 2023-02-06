f18ba50f99732cce3f891985b8887fd016926eac
JatinThakur <jtkalga@gmail.com>
2023-02-06 09:36:45 +0530

    Added: Loading Screen

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="loading-screen"></main>
+</template>
+<style scoped lang="scss">
+.loading-screen {
+  width: 10rem;
+  height: 10rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  translate: -50% -50%;
+  background-color: var(--color-text);
+  animation: anim infinite 2s;
+  @keyframes anim {
+    0% {
+      border-radius: 0;
+      rotate: 0;
+    }
+    50% {
+      border-radius: 50%;
+      rotate: 0.5turn;
+    }
+    100% {
+      border-radius: 0;
+      rotate: 0;
+    }
+  }
+}
+</style>
