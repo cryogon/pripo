e39a57b0d16f733ead6651adb44d81d672bbeced
JatinThakur <jtkalga@gmail.com>
2023-02-10 19:29:51 +0530

    Removed: Extra Code from setting

--- src/views/UserSettingPage.vue
+++ src/views/UserSettingPage.vue
@@ -1,23 +1,5 @@
-<script setup lang="ts">
-import { useRegisterSW } from "virtual:pwa-register/vue";
-const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
-
-const close = async () => {
-  offlineReady.value = false;
-  needRefresh.value = false;
-};
-</script>
+<script setup lang="ts"></script>
 <template>
-  <main v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
-    <div class="message">
-      <span v-if="offlineReady"> App ready to work offline </span>
-      <span v-else>
-        New content available, click on reload button to update.
-      </span>
-    </div>
-    <button v-if="needRefresh" @click="updateServiceWorker()">Reload</button>
-    <button @click="close">Close</button>
-  </main>
-  <main role="alert" v-else>Nothing Here</main>
+  <main>Nothing Here</main>
 </template>
 <style scoped lang="scss"></style>
