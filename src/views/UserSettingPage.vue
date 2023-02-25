063ecb1296cbb43258fc47191e914226487bc0b9
JatinThakur <jtkalga@gmail.com>
2023-02-25 22:41:30 +0530

    removed: test env var

--- src/views/UserSettingPage.vue
+++ src/views/UserSettingPage.vue
@@ -62,10 +62,7 @@ async function uploadImage() {
   });
   return image.data;
 }
-//TODO: Must Be removed when depoloyed to production
-function test() {
-  console.log(import.meta.env.VITE_IMG_UPLOAD_PATH);
-}
+
 function updateImage() {
   isImageUploading.value = true;
   uploadImage()
@@ -111,14 +108,6 @@ function clearImage() {
 function changeUsername() {
   emitter.emit("alert", "Changing username is not allowed yet!");
 }
-
-//TODO: Must Be removed when depoloyed to production
-document.addEventListener("keydown", (e) => {
-  e.preventDefault();
-  if (e.ctrlKey && e.key == "k") {
-    test();
-  }
-});
 // function changeFullName() {}
 </script>
 <template>
