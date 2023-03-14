c6c05d3d6924e6923bbc9515d3be630f265f3075
Jatin Thakur <jtkalga@gmail.com>
2023-03-14 19:14:46 +0530

    fix: comment can be liked multiple times

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,39 @@
+import AppIcon from "./AppIcon.vue";
+import BellIcon from "./BellIcon.vue";
+import CheckIcon from "./CheckIcon.vue";
+import EditIcon from "./EditIcon.vue";
+import HamBurger from "./HamBurger.vue";
+import HeartIcon from "./HeartIcon.vue";
+import LinkIcon from "./LinkIcon.vue";
+import LocationPin from "./LocationPin.vue";
+import MessageIcon from "./MessageIcon.vue";
+import OptionsIcon from "./OptionsIcon.vue";
+import PencilIcon from "./PencilIcon.vue";
+import PostIcon from "./PostIcon.vue";
+import ReplyIcon from "./ReplyIcon.vue";
+import SearchIcon from "./SearchIcon.vue";
+import ShareIcon from "./ShareIcon.vue";
+import StarIcon from "./StarIcon.vue";
+import ThumbsUp from "./ThumbsUp.vue";
+import XIcon from "./XIcon.vue";
+
+export {
+  AppIcon,
+  BellIcon,
+  CheckIcon,
+  EditIcon,
+  HamBurger,
+  HeartIcon,
+  LinkIcon,
+  LocationPin,
+  MessageIcon,
+  OptionsIcon,
+  PencilIcon,
+  PostIcon,
+  ReplyIcon,
+  SearchIcon,
+  ShareIcon,
+  StarIcon,
+  ThumbsUp,
+  XIcon,
+};
