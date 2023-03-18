06ae8390d80312aaca8c7c70f329e1bc51d44336
Jatin Thakur <jtkalga@gmail.com>
2023-03-18 15:43:20 +0530

    new: after sign in user is now redirect to where he was not the origin

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -4,7 +4,10 @@ import { useAuth0 } from "@auth0/auth0-vue";
 const { loginWithRedirect } = useAuth0();
 
 const handleLogin = async () => {
-  loginWithRedirect({ redirect_uri: window.location.origin });
+  loginWithRedirect({
+    redirect_uri: window.location.origin,
+    appState: { target: window.location.pathname },
+  });
 };
 </script>
 
